f8a402902b4cda9412f8eaa70394e32c0f6afa38
Rens Sibarani <renssibarani@gmail.com>
2023-11-20 23:03:15 +0700

    jika ini

---
+++ jika.java
@@ -0,0 +1,8 @@
+public class jika {
+    public static void main(String[] args) {
+        String makan = "ayam";
+        if (makan=="ayam") {
+            System.out.println("yeayyyyy makan ayam");
+        }
+    }
+}
